3e794a86fcd94e14687487b6e0063e344fd2b7d5
Solomon Hawk <solomon.hawk@viget.com>
2014-12-29 09:37:49 -0500

    Update example

--- examples/index.js
+++ examples/index.js
@@ -1,8 +1,8 @@
-var objs = require('../lib/object-sort');
+var objs = require('../dist/object-sort');
 var sort = objs.factory;
 
 // some fake data
-var data = require('./data.json');
+var pokemon = require('./data.json');
 
 // set up some sorting types, supplying the value getter function
 var types = {
@@ -21,11 +21,28 @@ objs.configure(types);
 var t = objs.types;
 
 // make a new sorter
-var pokemonSorter = sort(data.pokemon);
+var pokemonSorter = sort(pokemon);
 
 // chain methods to get the result you need
-var sortedByAttackInDescendingOrderAndGroupedByHp = pokemonSorter
+pokemonSorter
   .sort(t.sort.attack)
   .order(t.order.descending)
   .group(t.sort.hp)
   .getValue()
+  // => sorted by attack in descending order and grouped by hp
+
+pokemonSorter
+  .sort(t.sort.name)
+  .group(t.sort.types)
+  .tap(function(value) {
+    // do something with the value
+  })
+  .ungroup()
+  .sort(t.sort.hp)
+  .order(t.order.descending)
+  .tap(function(value) {
+    // do something with the value
+  })
+  .reset() // revert to the original list
+  ...
+
